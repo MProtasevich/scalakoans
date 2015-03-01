cb05164ca75f37379b5e387c5f662a11df712c5f
Max Protasevich <maksim.protasevich@gmail.com>
2015-03-01 19:50:40 +0300

    Advanced Options

--- src/test/scala/org/functionalkoans/forscala/AboutAdvancedOptions.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutAdvancedOptions.scala
@@ -4,34 +4,30 @@ import org.functionalkoans.forscala.support.KoanSuite
 
 class AboutAdvancedOptions extends KoanSuite {
   koan("Option is more than just a replacement of null, its also a collection") {
-    Some(10) map { _ + 10} should be(__)
-    Some(10) filter { _ == 10} should be(__)
-    Some(Some(10)) flatMap { _ map { _ + 10}} should be(__)
+    Some(10) map { _ + 10} should be(Some(20))
+    Some(10) filter { _ == 10} should be(Some(10))
+    Some(Some(10)) flatMap { _ map { _ + 10}} should be(Some(20))
 
     var newValue1 = 0
     Some(20) foreach { newValue1 = _}
-    newValue1 should be(__)
+    newValue1 should be(20)
 
     var newValue2 = 0
     None foreach { newValue2 = _}
-    newValue2 should be(__)
+    newValue2 should be(0)
   }
 
   koan("Using Option to avoid if checks for null") {
     //the ugly version
     def makeFullName(firstName: String, lastName: String) = {
-      if (firstName != null) {
-        if (lastName != null) {
+      if (firstName != null && lastName != null) {
           firstName + " " + lastName
-        } else {
-          null
-        }
       } else {
         null
       }
     }
-    makeFullName("Nilanjan", "Raychaudhuri") should be(__)
-    makeFullName("Nilanjan", null) should be(__)
+    makeFullName("Nilanjan", "Raychaudhuri") should be("Nilanjan Raychaudhuri")
+    makeFullName("Nilanjan", null) should be(null)
 
     //the pretty version
     def makeFullNamePrettyVersion(firstName: Option[String], lastName: Option[String]) = {
@@ -43,8 +39,8 @@ class AboutAdvancedOptions extends KoanSuite {
           }
       }
     }
-    makeFullNamePrettyVersion(Some("Nilanjan"), Some("Raychaudhuri")) should be(__)
-    makeFullNamePrettyVersion(Some("Nilanjan"), None) should be(__)
+    makeFullNamePrettyVersion(Some("Nilanjan"), Some("Raychaudhuri")) should be(Some("Nilanjan Raychaudhuri"))
+    makeFullNamePrettyVersion(Some("Nilanjan"), None) should be(None)
   }
 
   koan("Using in for comprehension") {
@@ -53,6 +49,6 @@ class AboutAdvancedOptions extends KoanSuite {
       someValue <- values
       value <- someValue
     } yield value
-    newValues should be(List(__, __, __))
+    newValues should be(List(10, 20, 15))
   }
 }
